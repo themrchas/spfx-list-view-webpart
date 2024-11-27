5efaad15be702cfa54240142c8461e3012842575
Charles Geib <charles.geib@devops.ncia.nato.int>
2024-11-27 12:11:27 +0100

    initial push

---
+++ src/webparts/listViewSp2019/ListViewSp2019WebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './ListViewSp2019WebPart.module.scss';
+import * as strings from 'ListViewSp2019WebPartStrings';
+
+export interface IListViewSp2019WebPartProps {
+  description: string;
+}
+
+export default class ListViewSp2019WebPart extends BaseClientSideWebPart<IListViewSp2019WebPartProps> {
+
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${ styles.listViewSp2019 }">
+        <div class="${ styles.container }">
+          <div class="${ styles.row }">
+            <div class="${ styles.column }">
+              <span class="${ styles.title }">Welcome to SharePoint!</span>
+              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
+              <p class="${ styles.description }">${escape(this.properties.description)}</p>
+              <a href="https://aka.ms/spfx" class="${ styles.button }">
+                <span class="${ styles.label }">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/listViewSp2019/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IListViewSp2019WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ListViewSp2019WebPartStrings' {
+  const strings: IListViewSp2019WebPartStrings;
+  export = strings;
+}
